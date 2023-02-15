9f78db72488d6c2dfdd1e84ab19b9fe179c140c2
icy9989 <kmhtwew1999@gmail.com>
2023-02-15 13:41:04 +0630

    second commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import { app } from "./firebaseConfig";
 const auth = getAuth(app);
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      input : ""
+    };
+  }
   captchaVerify = () => {
       window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
         'size': 'normal',
         'callback': (response) => {
-          console.log(response)
         },
         'expired-callback': () => {
           // Response expired. Ask user to solve reCAPTCHA again.
@@ -23,18 +28,22 @@ class App extends Component {
     this.captchaVerify();
   }
 
+  changeInput = (input) => {
+    this.setState({
+      input
+    })
+  }
+
   render() {
     return (
       <>
         <label>Phone Number</label>
-        <input />
+        <input type="text" placeholder="phone number" value={this.state.input} onChange={(e)=>this.changeInput(e.target.value)} />
         <div id="recaptcha-container" />
         <button id="sign-in-button">Send Code</button>
       </>
     );
   }
 }
-
-
 
 export default App;
